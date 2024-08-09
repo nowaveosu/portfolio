2bf13656b6284ff86b83e18c6cf7512007b09d70
nowaveosu <nowaveosu@gmail.com>
2024-08-09 23:58:11 +0900

    [Design] 배경색 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Introduction from './pages/Introduction';
 import Education from './pages/Education';
