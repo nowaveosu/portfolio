4a05d140e12bc0d48f5efc3661f8956390117e11
nowaveosu <nowaveosu@gmail.com>
2024-08-15 19:39:03 +0900

    [Add] 팀원평가 추가

--- src/pages/Evaluation.tsx
+++ src/pages/Evaluation.tsx
@@ -1,16 +1,35 @@
 import { Link } from "react-router-dom";
 import prev_button from "../images/prev_button.png"
 import next_button from "../images/next_button.png"
+import yeom from "../images/yeom.jpg";
+import joh from "../images/joh.png";
+import kim from "../images/kim.png";
 import "../GlobalStyle.css"
 
 function Evaluation() {
     return (
       <div>
         <Link to="/education"><img className="fixedPrev" src={prev_button} alt="prev button"/></Link>
-        <div className="content-box">3</div>
+        <div className="content-box">
+          <div className="large-div">팀원평가</div>
+          <div>
+            <div><img className="teampic" src={yeom} alt="yeom"></img>[FE] 염도경 </div>
+            <div style={{marginBottom:"7px"}}>호준님은 팀원들의 의견을 조용히 듣다가 적절한 중재안을 잘 말해줍니다. 백앤드와 소통해야 할 때도 굉장히 능숙합니다.</div>
+          </div>
+          <div>
+            <div><img className="teampic" src={joh} alt="joh"></img>[BE] 조만기</div>
+            <div style={{marginBottom:"7px"}}>모르는 분야를 같이 고민해주는 팀원이고, 어색할 때 부드럽게 분위기를 풀어주는 사람이다.</div>
+          </div>
+          <div>
+            <div><img className="teampic" src={kim} alt="kim"></img>[FE] 김성수</div>
+            <div style={{marginBottom:"7px"}}>호준님은 코딩 능력이 뛰어나지만, 우쭐대지 않고 팀원을 존중하고 소통시에 상대방의 의견을 수용하려 노력한다. 그러나, 의견이 많아지면 약간 어지러워 한다.</div>
+          </div>
+          
+        </div>
         <Link to="/experience"><img className="fixedNext" src={next_button} alt="next button"/></Link>
       </div>
     );
   }
   
   export default Evaluation;
+
--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -7,7 +7,16 @@ function Experience() {
     return (
       <div>
         <Link to="/evaluation"><img className="fixedPrev" src={prev_button} alt="prev button"/></Link>
-        <div className="content-box">4</div>
+        <div className="content-box">
+          <div className="large-div">학업</div>
+          <div>2017-2023</div>
+          <div className="weight-div" style={{marginBottom:"5px"}}>전남대학교 컴퓨터정보통신공학과 전공</div>
+          <div style={{marginBottom:"18px"}}>컴퓨터공학과 전공자로, 재학중 2번의 앱 서비스 개발 및 해커톤에 참여, 1회 수상
+          4학년 학부연구생으로서 Python을 활용한 자동 채점 시스템 개발</div>
+          <div className="large-div">자격증</div>
+          <div>Toeic : 790</div>
+          <div>정보처리기사: 필기합격 (2024년 3회차)</div>
+        </div>
         <Link to="/projects"><img className="fixedNext" src={next_button} alt="next button"/></Link>
       </div>
     );
