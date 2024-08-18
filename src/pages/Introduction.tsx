aafd494429d42a0663ae0509f63d7f0cdf568aa7
nowaveosu <nowaveosu@gmail.com>
2024-08-18 15:23:41 +0900

    [Modify] about 수정

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -18,7 +18,7 @@ function Introduction() {
                 <li className="weight-div"><span>Phone: </span> <a href="tel://01073513804">010-7351-3804</a></li>
                 <li className="weight-div"><span>github:</span> <a href="https://github.com/nowaveosu">github.com/nowaveosu</a></li>
                 <li className="weight-div"><span>blog: </span> <a href="https://velog.io/@nowaveosu">velog.io/@nowaveosu</a></li>
-                <li><span className="weight-div">다짐: </span> 언젠가 저만의 웹사이트로 꾸준한 수익을 얻고싶습니다. 현재 프론트엔드 개발자로 구직중입니다.</li>
+                <li><span className="weight-div">다짐: </span> 명확한 커뮤니케이션을 통해 소통하고, 긍정적인 마음으로 개발에 임하겠습니다. 현재 프론트엔드 개발자로 구직중입니다.</li>
           </ul>
         </div>
       </div>
