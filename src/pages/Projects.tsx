7d0fa52e52081167f6925ac39b67b1683957fa63
nowaveosu <nowaveosu@gmail.com>
2024-08-18 14:31:50 +0900

    [Design] 버튼색변경

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -12,8 +12,8 @@ function Projects() {
             <div className="project">
               <div>2024.07</div>
               <div><div className="large-div">[hyper-tictactoe] 독특한 룰을 여러개 적용한 1인개발 온라인 매칭 틱택토게임&nbsp;
-                <button><a href="https://github.com/nowaveosu/hyper-tictactoe" target="_blank" rel="noreferrer">Github</a></button>
-                <button><a href="https://www.hyper-tictactoe.com/" target="_blank" rel="noreferrer">Web</a></button></div></div>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://github.com/nowaveosu/hyper-tictactoe" target="_blank" rel="noreferrer">Github</a></button>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://www.hyper-tictactoe.com/" target="_blank" rel="noreferrer">Web</a></button></div></div>
               <div style={{marginBottom:"7px"}}><span className="emp">기술 스택 :</span> Next.js, TypeScript, Tailwind CSS, Socket.io, Node.js</div>
               <div style={{marginBottom:"7px"}}><span className="emp">담당기능 :</span> 로고, 반응형 디자인, 게임 룰 개발, 실시간 채팅 및 게임의 프론트엔드, 백엔드 구현, 디자인, 개발, 배포 모든 단계를 독자적으로 진행했음</div>
               <div className="emp" style={{marginBottom:"-17px"}}>시연영상 📼</div>
@@ -33,8 +33,8 @@ function Projects() {
             <div className="project">
               <div>2023.07</div>
               <div><div className="large-div">[펫칭] 펫시터와 반려동물 주인 매칭 웹 서비스&nbsp;
-                <button><a href="https://github.com/Petching/Petching" target="_blank" rel="noreferrer">Github</a></button>
-                <button><a href="https://petching.net/" target="_blank" rel="noreferrer">Web</a></button></div></div>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://github.com/Petching/Petching" target="_blank" rel="noreferrer">Github</a></button>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://petching.net/" target="_blank" rel="noreferrer">Web</a></button></div></div>
               <div style={{marginBottom:"7px"}}><span className="emp">기술 스택 :</span> React, TypeScript, Zustand, tailwind-CSS</div>
               <div style={{marginBottom:"7px"}}><span className="emp">담당기능 :</span> Tailwind CSS를 활용한 돌봄리스트 페이지 및 글작성 페이지의 반응형 디자인 구현, 게시글 CRUD 구현, 카카오 주소 API를 이용한 검색 기능 구현</div>
               <div className="emp" style={{marginBottom:"-17px"}}>시연영상 📼</div>
@@ -54,8 +54,8 @@ function Projects() {
             <div className="project">
               <div>2023.05</div>
               <div><div className="large-div">[도와조약] 사용자 알러지 기반 알약 추천 웹 서비스&nbsp;
-                <button><a href="https://github.com/codestates-seb/seb43_main_009" target="_blank" rel="noreferrer">Github</a></button>
-                <button><a href="https://dowajoyak.store" target="_blank" rel="noreferrer">Web</a></button></div></div>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://github.com/codestates-seb/seb43_main_009" target="_blank" rel="noreferrer">Github</a></button>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://dowajoyak.store" target="_blank" rel="noreferrer">Web</a></button></div></div>
               <div style={{marginBottom:"7px"}}><span className="emp">기술 스택 :</span> React, JavaScript, Redux, Styled-Component</div>
               <div style={{marginBottom:"7px"}}><span className="emp">담당기능 :</span> 헤더, 로그인, 회원가입, OAuth, 맞춤추천 페이지 개발 및 구글검색 API 사용과 디자인 구현</div>
               <div className="emp" style={{marginBottom:"-17px"}}>시연영상 📼</div>
@@ -71,7 +71,7 @@ function Projects() {
             <div className="project">
               <div>2021.10</div>
               <div><div className="large-div">[해커톤: 같이탈래] 같은 경로를 이용하는 택시 이용자를 매칭하는 어플리케이션&nbsp;
-                <button><a href="https://github.com/nowaveosu/gachiTarlae" target="_blank" rel="noreferrer">Github</a></button>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://github.com/nowaveosu/gachiTarlae" target="_blank" rel="noreferrer">Github</a></button>
                 </div></div>
               <div style={{marginBottom:"7px"}}><span className="emp">기술 스택 :</span> flutter, firebase</div>
               <div style={{marginBottom:"7px"}}><span className="emp">담당기능 :</span> flutter을 사용한 프론트엔드 개발 및 Google Map API 연동</div>
@@ -79,8 +79,8 @@ function Projects() {
             <div className="project">
               <div>2021.08</div>
               <div><div className="large-div">[토이 프로젝트] 손에 맞는 마우스 추천 사이트&nbsp;
-                <button><a href="https://github.com/nowaveosu/lifemouse" target="_blank" rel="noreferrer">Github</a></button>
-                <button><a href="https://nowaveosu.github.io/lifemouse/welcome.html" target="_blank" rel="noreferrer">Link</a></button>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://github.com/nowaveosu/lifemouse" target="_blank" rel="noreferrer">Github</a></button>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://nowaveosu.github.io/lifemouse/welcome.html" target="_blank" rel="noreferrer">Link</a></button>
                 </div></div>
               <div style={{marginBottom:"7px"}}><span className="emp">기술 스택 :</span> HTML</div>
               <div style={{marginBottom:"7px"}}><span className="emp">담당기능 :</span> 대학교 3학년 때 HTML을 독학하며 정적 웹사이트 개발</div>
