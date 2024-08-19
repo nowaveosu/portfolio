c3800597add2b350aea9a3e43a49097a66365609
nowaveosu <nowaveosu@gmail.com>
2024-08-19 19:44:28 +0900

    [Design] weight 변경

--- src/pages/Introduction.tsx
+++ src/pages/Introduction.tsx
@@ -13,11 +13,11 @@ function Introduction() {
           <img className="about" src={about} alt ="프사"></img>
           <ul className="about-ul">
                 <li className="large-div">소개</li>
-                <li className="weight-div"><span>Name:</span> <a href="tel://01073513804">노호준</a></li>
-                <li className="weight-div"><span>Email:</span> <a href="mailto:nowaveosu@gmail.com">nowaveosu@gmail.com</a></li>
-                <li className="weight-div"><span>Phone: </span> <a href="tel://01073513804">010-7351-3804</a></li>
-                <li className="weight-div"><span>github:</span> <a href="https://github.com/nowaveosu">github.com/nowaveosu</a></li>
-                <li className="weight-div"><span>blog: </span> <a href="https://velog.io/@nowaveosu">velog.io/@nowaveosu</a></li>
+                <li><span className="weight-div">Name:</span> <a href="tel://01073513804">노호준</a></li>
+                <li><span className="weight-div">Email:</span> <a href="mailto:nowaveosu@gmail.com">nowaveosu@gmail.com</a></li>
+                <li><span className="weight-div">Phone: </span> <a href="tel://01073513804">010-7351-3804</a></li>
+                <li><span className="weight-div">github:</span> <a href="https://github.com/nowaveosu">github.com/nowaveosu</a></li>
+                <li><span className="weight-div">blog: </span> <a href="https://velog.io/@nowaveosu">velog.io/@nowaveosu</a></li>
                 <li><span className="weight-div">다짐: </span> 명확한 커뮤니케이션을 통해 소통하고, 긍정적인 마음으로 개발에 임하겠습니다. 현재 프론트엔드 개발자로 구직중입니다.</li>
           </ul>
         </div>
