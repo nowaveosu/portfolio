8820bd6bd5efd405ce151c0957c3b34f77ef70de
nowaveosu <nowaveosu@gmail.com>
2024-09-19 21:39:02 +0900

    [Fix] 문구수정

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -10,7 +10,7 @@ function Contact() {
         <Link to="/projects"><img className="fixedPrev"src={prev_button} alt="prev_button"/></Link>
         <div className="content-box-small">
           <div style={{padding: 40}}>
-            <div className="large-div">회사와 함께 성장하는 개발자가 되겠습니다. 연락주세요.</div>
+            <div className="large-div">회사의 발전에 기여하며 함께 성장하는 개발자가 되고 싶습니다. 연락 기다리겠습니다.</div>
             <div className="weight-div">Contact Number: <a href="tel://01073513804">010 7351 3804</a></div>
             <div className="weight-div">Mail: <a href="mailto:nowaveosu@gmail.com">nowaveosu@gmail.com</a></div>
             <div className="weight-div">Github: <a href="https://github.com/nowaveosu">github.com/nowaveosu</a></div>
