49ada049a82798ae782e4b7d9a4d574199e07db8
nowaveosu <nowaveosu@gmail.com>
2025-03-06 13:55:41 +0900

    [Add] project 추가

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,6 +9,23 @@ function Projects() {
         <Link to="/experience"><img className="fixedPrev" src={prev_button} alt="prev button"/></Link>
         <div className="content-box-small">
           <div className="large-div" style={{marginTop: 20, marginBottom: 30}}>프로젝트</div>
+          <div className="project">
+              <div>2025.03</div>
+              <div><div className="large-div">[가려움없는세상] 아토피 환자 정보공유 게시판&nbsp;
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://github.com/nowaveosu/muri-shop" target="_blank" rel="noreferrer">Github</a></button>
+                <button><a style={{color: "rgb(126, 206, 249)"}} href="https://nomoreitch.vercel.app/" target="_blank" rel="noreferrer">Web</a></button></div><hr /><br /></div>
+              <div style={{marginBottom:"7px"}}><span className="emp">기술 스택 :</span> Next.JS, Redux, MongoDB, TypeScript</div>
+              <div style={{marginBottom:"7px"}}><span className="emp">담당기능 :</span> 반응형디자인, 프론트엔드, 게시판, 댓글, DB및 서버구현 1인 프로젝트로 진행했음</div>
+              <div className="emp" style={{marginBottom:"-17px"}}>시연영상 📼</div>
+              <video width="100%" controls>
+                <source src={require("../videos/lotion.mp4")} type="video/mp4"/>
+              </video>
+              <div>로그인, 제품DB구성, 좋아요, 댓글기능 구현 </div>
+              <video width="100%" controls>
+                <source src={require("../videos/board.mp4")} type="video/mp4"/>
+              </video>
+              <div>말머리를 정하고 게시판에 글을 자유롭게 올리고 회원/비회원 차이를 둠, 댓글기능 구현</div>
+            </div>
             <div className="project">
               <div>2024.07</div>
               <div><div className="large-div">[hyper-tictactoe] 독특한 룰을 여러개 적용한 1인개발 온라인 매칭 틱택토게임&nbsp;
