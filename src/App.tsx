a2932e9e301d6132140261f8a990348682f0b717
nowaveosu <nowaveosu@gmail.com>
2025-04-14 18:08:50 +0900

    [Fix] 라우팅 index 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Welcome from './pages/Welcome'
 import Layout from "./Layout";
 import Introduction from './pages/Introduction';
@@ -11,9 +11,10 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}> 
-          <Route path="/" element={<Welcome/>}></Route>
+          <Route index element={<Welcome/>}></Route>
           <Route path="/introduction" element={<Introduction/>}></Route>
           <Route path="/education" element={<Education/>}></Route>
           <Route path="/evaluation" element={<Evaluation/>}></Route>
@@ -21,8 +22,9 @@ function App() {
           <Route path="/projects" element={<Projects/>}></Route>
           <Route path="/contact" element={<Contact/>}></Route>
         </Route>
+      </Routes>    
+    </BrowserRouter>
 
-      </Routes>
   );
 }
 
